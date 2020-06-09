caca1566d91f8c2ff922d2a45f9c4194aaba1256
Facico <845161327@qq.com>
2020-06-09 18:16:09 +0800

    生成验证集中ETA改成只生成有ETA的，因为A榜测试集都有

--- generate_test.py
+++ generate_test.py
@@ -13,14 +13,12 @@ import random
 warnings.filterwarnings('ignore')
 
 # baseline只用到gps定位数据，即train_gps_path
-os.chdir("E:\华为云")
-train_gps_path = 'data/train0523.csv'
-test_data_path = 'data/A_testData0531.csv'
-self_test_data_path = 'data/self_test.csv'
-order_data_path = 'data/loadingOrderEvent.csv'
-port_data_path = 'data/port.csv'
-split_data_path='data/split'
-feature_path='src/data/features.csv'
+train_gps_path = './data/train0523.csv'
+test_data_path = './data/A_testData0531.csv'
+self_test_data_path = './data/self_test.csv'
+order_data_path = './data/loadingOrderEvent.csv'
+port_data_path = './data/port.csv'
+split_data_path='./data/split'
 header_flag=True
 
 def get_data(file):
@@ -29,6 +27,7 @@ def get_data(file):
     train_data.columns = ['id','loadingOrder','carrierName','timestamp','longitude',
                     'latitude','vesselMMSI','speed','direction','vesselNextport',
                     'vesselNextportETA','vesselStatus','vesselDatasource','TRANSPORT_TRACE']
+    if (str(train_data['TRANSPORT_TRACE'][0]) == 'nan'): return False
     n=len(train_data["id"])
     k=int(random.uniform(10,30)*n//100)
     train_data['onboardDate']=''
@@ -39,7 +38,7 @@ def get_data(file):
     test_data.to_csv(self_test_data_path,index=False,mode='a',header=header_flag)
     if(header_flag):
         header_flag=False
-
+    return True
 
 file_list = os.listdir(split_data_path)
 cnt=0
@@ -48,7 +47,7 @@ if os.path.exists(self_test_data_path):
 while True:
     file=random.choice(file_list)
     print(file)
-    get_data(file)
-    cnt+=1
+    ff = get_data(file)
+    if (ff == True): cnt+=1
     if(cnt==100):
         break
